9da39d9d2e849d774f686199f98773d51b8c4ffa
Dan Regev <dan.regev@mail.huji.ac.il>
2019-11-20 21:49:33 +0200

    Enough for today

--- TreeAnalyzer.c
+++ TreeAnalyzer.c
@@ -5,7 +5,6 @@
 
 #define ARG_ERR_MSG "Usage: TreeAnalyzer <Graph File Path> <First Vertex> <Second Vertex>\n"
 #define INVALID_INPUT_MSG "Invalid input\n"
-
 #define MAX_LINE_LENGTH 1024
 struct Vertex
 {
@@ -13,9 +12,16 @@ struct Vertex
     struct Vertex *children;
 };
 
+int isDigit(char c)
+{
+    return c >= 48 && c <= 57;
+}
+
 int parseFile(const char *filename)
 {
     FILE *fp = fopen(filename, "r");
+
+    //Check if file exists or if it is empty:
     if (fp == NULL)
     {
         fprintf(stderr, INVALID_INPUT_MSG);
@@ -24,9 +30,19 @@ int parseFile(const char *filename)
     }
 
     char *line = NULL;
+
+    //Check if first line contains an integer number:
+    fgets(line, MAX_LINE_LENGTH, fp);
+    for (int i = 0; i < MAX_LINE_LENGTH && line[i] != '\0'; i++)
+    {
+        if (!isDigit(line[i]))
+        {
+
+        }
+    }
     while (fgets(line, MAX_LINE_LENGTH, fp) != NULL)
     {
-        
+
     }
 
     fclose(fp);
