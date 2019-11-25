b3f7e3243d710c867957932e149f3e252134e850
Dan Regev <dan.regev@mail.huji.ac.il>
2019-11-25 13:49:13 +0200

    BFS works

--- TreeAnalyzer.c
+++ TreeAnalyzer.c
@@ -8,7 +8,6 @@
 #define MAX_LINE_LENGTH 1025
 #define MAX_CHILDREN 512
 #define SUCCESS 0
-#define NO_PARENT_FLAG (-1)
 #define LEAF_FLAG (-2)
 #define NOT_VISITED (-1)
 #define FST_VERTEX_PREV (-1)
@@ -19,6 +18,7 @@
 struct Vertex
 {
     int neighbors[MAX_CHILDREN + 1];
+    struct Vertex *father;
     int key;
     int distance;
     unsigned int prev;
@@ -35,7 +35,7 @@ struct Vertex *getRoot(int vertexCounter, struct Vertex *const *vertices, struct
 {
     for (int k = 0; k < vertexCounter; k++)
     {
-        if (vertices[k]->neighbors[0] == NO_PARENT_FLAG)
+        if (vertices[k]->father == NULL)
         {
             *root = vertices[k];
         }
@@ -133,7 +133,7 @@ int isInCheckedTokens(char *checkedTokens[], char *token, int length)
  * @param vertexCounter
  * @param root
  */
-void printDetails(int fstVertex, int secVertex, int vertexCounter, const struct Vertex *root)
+void printDetails(char *fstVertex, char *secVertex, int vertexCounter, const struct Vertex *root)
 {
 	fprintf(stdout, "Root Vertex: %d\n", root->key);
 	fprintf(stdout, "Vertices Count: %d\n", vertexCounter);
@@ -141,21 +141,26 @@ void printDetails(int fstVertex, int secVertex, int vertexCounter, const struct 
 //    fprintf(stdout, "Length of Minimal Branch: %d\n");
 //    fprintf(stdout, "Length of Maximal Branch: %d\n");
 //    fprintf(stdout, "Diameter Length: %d\n");
-	fprintf(stdout, "Shortest Path Between %d and %d: ", fstVertex, secVertex);
+	fprintf(stdout, "Shortest Path Between %s and %s: ", fstVertex, secVertex);
 }
 
-//void destroyVertices(int vertexCounter, struct Vertex **vertices)
-//{
-//    for (int j = 0; j < vertexCounter; j++)
-//    {
-//        free(vertices[j]);
-//        vertices[j] = NULL;
-//    }
-//    free(vertices);
-//    vertices = NULL;
-//}
+void destroyVertices(int vertexCounter, struct Vertex **vertices)
+{
+    for (int j = 0; j < vertexCounter; j++)
+    {
+        free(vertices[j]);
+        vertices[j] = NULL;
+    }
+    free(vertices);
+    vertices = NULL;
+}
 
-
+/**
+ *
+ * @param vertices
+ * @param key
+ * @param vertexCounter
+ */
 void bfs(struct Vertex *const *vertices, int key, int vertexCounter)
 {
 	for (int i = 0; i < vertexCounter; i++)
@@ -172,12 +177,23 @@ void bfs(struct Vertex *const *vertices, int key, int vertexCounter)
 		for (int i = 0; i < MAX_CHILDREN + 1 && vertices[u]->neighbors[i] != END_OF_NEIGHBORS; i++)
 		{
 			int w = vertices[u]->neighbors[i];
-			if (vertices[w]->distance == NOT_VISITED)
-			{
-				enqueue(queue, w);
-				vertices[w]->prev = u;
-				vertices[w]->distance = vertices[u]->distance + 1;
-			}
+			if (w == LEAF_FLAG)
+            {
+			    if (vertices[u]->father->distance == NOT_VISITED)
+                {
+                    enqueue(queue, vertices[u]->father->key);
+                    vertices[u]->father->prev = u;
+                    vertices[u]->father->distance = vertices[u]->distance + 1;
+                }
+            } else
+              {
+                  if (vertices[w]->distance == NOT_VISITED)
+                  {
+                      enqueue(queue, w);
+                      vertices[w]->prev = u;
+                      vertices[w]->distance = vertices[u]->distance + 1;
+                  }
+              }
 		}
 	}
 }
@@ -187,7 +203,7 @@ void bfs(struct Vertex *const *vertices, int key, int vertexCounter)
  * @param filePointer
  * @return
  */
-int parseFile(FILE *filePointer)
+int parseFile(FILE *filePointer, char *fstVertex, char *secVertex)
 {
     //Edge case: Check if file exists:
     if (filePointer == NULL)
@@ -237,7 +253,7 @@ int parseFile(FILE *filePointer)
     rewind(filePointer);
     fgets(line, MAX_LINE_LENGTH, filePointer);
 
-    int key = 0, tokenCounter = 1;
+    int key = 0, tokenCounter = 1, i = 0;
     char *checkedTokens[MAX_CHILDREN] = {NULL};
     while (fgets(line, MAX_LINE_LENGTH, filePointer) != NULL)
     {
@@ -246,11 +262,9 @@ int parseFile(FILE *filePointer)
         if (strncmp(token, "-", 1) == 0)
         {
             key++;
-            fprintf(stderr, "%s", line);
             continue;
         }
 
-        int i = 0;
         while (token != NULL)
         {
             int tokenInteger = (int)strtol(token, NULL, 10);
@@ -265,13 +279,38 @@ int parseFile(FILE *filePointer)
             i++;
         }
         key++;
-        fprintf(stderr, "%s\n", line);
     }
 
     if (tokenCounter != vertexNum)
     {
         fprintf(stderr,
                 "NUMBER OF VERTEXES DOESN'T MATCH SPECIFIED NUMBER"); //TODO change to failureExit
+        return EXIT_FAILURE;
+    }
+
+    //Check if given vertices exist and if they are legal:
+
+    for (int j = 0; j < vertexNum && fstVertex[j] != '\0'; j++)
+    {
+        if (!isDigit(fstVertex[j]))
+        {
+            fprintf(stderr, "FIRST GIVEN VERTEX IS NOT AN INTEGER"); //TODO change to failureExit
+            return EXIT_FAILURE;
+        }
+    }
+    for (int j = 0; j < vertexNum && secVertex[j] != '\0'; j++)
+    {
+        if (!isDigit(secVertex[j]))
+        {
+            fprintf(stderr, "SECOND GIVEN VERTEX IS NOT AN INTEGER"); //TODO change to failureExit
+            return EXIT_FAILURE;
+        }
+    }
+    int vertex1 = (int)strtol(fstVertex, NULL, 10);
+    int vertex2 = (int)strtol(secVertex, NULL, 10);
+    if (vertex1 < 0 || vertex1 >= vertexNum || vertex2 < 0 || vertex2 >= vertexNum)
+    {
+        fprintf(stderr, "GIVEN VERTICES DO NOT EXIST IN TREE"); //TODO change to failureExit
         return EXIT_FAILURE;
     }
     return SUCCESS;
@@ -284,7 +323,7 @@ int parseFile(FILE *filePointer)
  * @param secVertex
  * @return
  */
-int processTree(FILE *filePointer, int fstVertex, int secVertex)
+int processTree(FILE *filePointer, char *fstVertex, char *secVertex)
 {
     char line[MAX_LINE_LENGTH];
 
@@ -296,8 +335,24 @@ int processTree(FILE *filePointer, int fstVertex, int secVertex)
     {
         fprintf(stderr, "MALLOC FAILED");
         free(vertices);
-        vertices = NULL;
         return EXIT_FAILURE;
+    }
+
+    for (int k = 0; k < vertexCounter; k++)
+    {
+        vertices[k] = (struct Vertex *) malloc(sizeof(struct Vertex));
+        if (vertices[k] == NULL)
+        {
+            fprintf(stderr, "MALLOC FAILED");
+            for (int i = 0; i < k; i++)
+            {
+                free(vertices[i]);
+                vertices[i] = NULL;
+            }
+            free(vertices);
+            vertices = NULL;
+            return EXIT_FAILURE;
+        }
     }
 
     //Add vertices to array:
@@ -305,41 +360,26 @@ int processTree(FILE *filePointer, int fstVertex, int secVertex)
     {
         fgets(line, MAX_LINE_LENGTH, filePointer);
 
-//        vertices[key] = (struct Vertex *) malloc(sizeof(struct Vertex));
-//        if (vertices[key] == NULL)
-//        {
-//            fprintf(stderr, "MALLOC FAILED");
-//            for (int i = 0; i < key; i++)
-//            {
-//                free(vertices[i]);
-//                vertices[i] = NULL;
-//            }
-//            free(vertices);
-//            vertices = NULL;
-//            return EXIT_FAILURE;
-//        }
-
         vertices[key]->key = key;
-        vertices[key]->neighbors[0] = NO_PARENT_FLAG;
 
         char *token = strtok(line, " ");
 
         //If vertex is a leaf:
         if (strncmp(token, "-", 1) == 0)
         {
-            vertices[key]->neighbors[1] = LEAF_FLAG;
+            vertices[key]->neighbors[0] = LEAF_FLAG;
             continue;
         }
 
         //Write neighbors of current vertex:
-        int i = 1;
+        int i = 0;
         while (token != NULL)
         {
             int tokenInteger = (int)strtol(token, NULL, 10);
-            vertices[key]->neighbors[i] = tokenInteger; //TODO check if initialised
+            vertices[key]->neighbors[i] = tokenInteger;
 
             //Write current key as father of current child:
-            vertices[tokenInteger]->neighbors[0] = key;
+            vertices[tokenInteger]->father = vertices[key];
 
             token = strtok(NULL, " ");
             i++;
@@ -351,12 +391,17 @@ int processTree(FILE *filePointer, int fstVertex, int secVertex)
     struct Vertex *root = NULL;
     root = getRoot(vertexCounter, vertices, &root);
 
+    bfs(vertices, root->key, vertexCounter);
+    for (int j = 0; j < vertexCounter; j++)
+    {
+        fprintf(stderr, "%d\n", vertices[j]->distance);
+    }
 
-    printDetails(fstVertex, secVertex, vertexCounter, root);
+//    printDetails(fstVertex, secVertex, vertexCounter, root);
     //Free memory of vertices:
     free(vertices);
     vertices = NULL;
-//    destroyVertices(vertexCounter, vertices);//TODO check if need to send pointer to pointer etc.
+    destroyVertices(vertexCounter, vertices);//TODO check if need to send pointer to pointer etc.
     return SUCCESS;
 }
 
@@ -375,18 +420,18 @@ int main(int argc, char *argv[])
     }
 
     FILE *filePointer = fopen(argv[1], "r");
-    if (parseFile(filePointer))
+    if (parseFile(filePointer, argv[2], argv[3]))
     {
         fclose(filePointer);
         return EXIT_FAILURE;
     }
 
     rewind(filePointer);
-//    if (processTree(filePointer))
-//    {
-//        fclose(filePointer);
-//        return EXIT_FAILURE;
-//    }
+    if (processTree(filePointer, argv[2], argv[3]))
+    {
+        fclose(filePointer);
+        return EXIT_FAILURE;
+    }
     fclose(filePointer);
     return SUCCESS;
 }
